4c8c038cc6cc6dd4d3286e29368ce775d90675dd
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-08 16:40:51 +0530

    Created layout file

---
+++ resources/views/partials/_layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @yield('pageTitle')
+    <meta name="theme-color" content="#a5b4fc">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}" />
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+
+    @yield('pageStyles')
+</head>
+<body class="bg-gray-900 tracking-widest text-white antialiased">
+    @yield('content')
+
+    @yield('pageScripts')
+</body>
+</html>
