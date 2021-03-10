f28364bb32f3f9b73166c7bc0fea93c96103903a
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-10 15:08:02 +0530

    Component for card skeleton

---
+++ resources/views/components/game-card-small-skeleton.blade.php
@@ -0,0 +1,8 @@
+<div class="game flex">
+    <div class="bg-gray-800 w-16 h-20 flex-none"></div>
+
+    <div class="ml-4">
+        <div class="text-transparent bg-gray-700 rounded leading-tight">Title Goes Here Today</div>
+        <div class="text-transparent bg-gray-700 rounded inline-block mt-2">Sept 14, 2021</div>
+    </div>
+</div>
